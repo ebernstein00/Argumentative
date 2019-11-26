ede238652ee091be6e3f25184348cdfdf351f664
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-11-26 17:18:13 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,25 +5,20 @@
 
 char ** parse_args(char *line){
   printf("test\n");
-  //int arsize = 6;
   char **args = malloc(5 * sizeof(char *));
   char *curr = line;
   int i = 0;
   while (*curr != '\0'){
     args[i++] = strsep(&curr, " \n");
-    /*if (i >= arsize){
-      arsize *= 2;
-      args = realloc(args, arsize * sizeof(char *));
-    }*/
   }
   args[i] = NULL;
   return args;
 }
 
 int main(){
-  printf("> ");
-  char buf[128];
-  fgets(buf,128,stdin);
+  //printf("> ");
+  char buf[128] = "ls -a -l";
+  //fgets(buf,128,stdin);
   char **args = parse_args(buf);
   //char c[100] = "ls -a -l";
   //char **args = parse_args(c);
