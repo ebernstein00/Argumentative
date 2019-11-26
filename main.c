e268610f9a1a9b2ed70dea17b2580d3c4d1d7b35
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-11-26 15:30:01 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char ** parse_args(char *line){
+  char **args = malloc(100);
+  char *curr = line;
+  char *token;
+  int i = 0;
+  while (curr != NULL){
+    token = strsep(curr, " ");
+    args[i] = token;
+    i += sizeof(token) + 1;
+  }
+}
+
+int main(){
+  char ** args = parse_args("ls -a -l");
+  execvp(args[0], args);
+}
