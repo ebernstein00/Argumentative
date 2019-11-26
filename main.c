464b15849563bb771c264c4fc755215629f652d5
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-11-26 16:54:02 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <string.h>
+/*#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -20,5 +20,37 @@ int main(){
   printf("test");
   char ** args = parse_args("ls -a -l");
   execvp(args[0], args);
+  free(args);
+}
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+
+char ** parse_args(char *line) {
+  unsigned short i;
+  unsigned short ary_size = 6; // originally i missed the specified max size, this works for larger amounts too
+  char **args = malloc(ary_size * sizeof(char *));
+  char *cur = line;
+  i = 0;
+  while(*cur != '\0') {
+    args[i++] = strsep(&cur," \t\n");
+    if(i >= ary_size) {
+      ary_size *= 2;
+      args = realloc(args,ary_size * sizeof(char *));
+    }
+  }
+  args[i] = NULL;
+  return args;
+}
+
+int main(){
+  printf("> ");
+  char buf[128];
+  fgets(buf,128,stdin);
+  char **args = parse_args(buf);
+  execvp(args[0],args);
   free(args);
 }
