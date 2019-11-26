8a8275ab8723c2feed0f1a5f668b25677c35e46a
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-11-26 17:24:26 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,39 +4,20 @@
 #include <unistd.h>
 
 char ** parse_args(char *line){
-  printf("test\n");
   char **args = malloc(5 * sizeof(char *));
   char *curr = line;
-  //char *token;
   int i = 0;
   while (*curr != '\0'){
-    //token = strsep(&curr, " ");
     args[i++] = strsep(&curr, " \n");
-    //printf("%s\n", token);
   }
   args[i] = NULL;
   return args;
 }
 
 int main(){
-  //printf("> ");
-  //char buf[128];
-  //fgets(buf,128,stdin);
-  //char **args = parse_args(buf);
+  printf("Testing: ls -a -l\n");
   char c[100] = "ls -a -l\n";
   char **args = parse_args(c);
   execvp(args[0], args);
   free(args);
 }
-
- /*
-
-int main(){
-  printf("> ");
-  char buf[128];
-  fgets(buf,128,stdin);
-  char **args = parse_args(buf);
-  execvp(args[0],args);
-  free(args);
-}
-*/
